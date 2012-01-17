871dbacfddf09953f5ec1ad4c81f4c8985842c15
charlesmarshall <cm56marshall@googlemail.com>
2012-01-17 10:35:11 +0000

    dont need that any more

--- lib/controller/CMSAdminNavigationController.php
+++ lib/controller/CMSAdminNavigationController.php
@@ -11,7 +11,6 @@ class CMSAdminNavigationController extends AdminContentController{
   public $filter_fields=array(
                           'text' => array('columns'=>array('title'), 'partial'=>'_filters_text', 'fuzzy'=>true),
                           'parent' => array('columns'=>array('parent_id'), 'partial'=>'_filters_parent'),
-                          'which' => array('columns'=>array('which_navigation'), 'partial'=>"_filters_grouped_column"),
                           'language' => array('columns'=>array('language'), 'partial'=>"_filters_language")
 	                      );
   
