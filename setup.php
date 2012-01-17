b2319e378f97ce017a96cd8b4ed7e31b070813a7
charlesmarshall <cm56marshall@googlemail.com>
2012-01-17 12:11:01 +0000

    figure out the content model class being used and hook into the setup event to add in the nav join

--- setup.php
+++ setup.php
@@ -1,3 +1,13 @@
 <?
 CMSApplication::register_module("navigation", array("display_name"=>"Navigation", "link"=>"/admin/navigation/", 'split'=>true));
+
+if(!defined("CONTENT_MODEL")){
+  $con = new ApplicationController(false, false);
+  define("CONTENT_MODEL", $con->cms_content_class);
+}
+
+WaxEvent::add(CONTENT_MODEL.".setup", function(){
+  $model = WaxEvent::data();
+  if(!$model->columns['navigation_items']) $model->define("navigation_items", "ManyToManyField", array('target_model'=>'WildfireNavigationItem', 'group'=>'relationships'));
+});
 ?>
