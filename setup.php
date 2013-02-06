75428073aa6c9119a414464b11edc82c00a9d122
Charles Marshall <charles@oneblackbear.com>
2013-02-06 09:45:51 +0000

    composer setup

--- setup.php
+++ setup.php
@@ -1,6 +1,12 @@
 <?
 CMSApplication::register_module("navigation", array("display_name"=>"Navigation", "link"=>"/admin/navigation/", 'split'=>true));
 
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.categories","dir"=>__DIR__);
+
+
 if(defined("CONTENT_MODEL")){
   WaxEvent::add(CONTENT_MODEL.".setup", function(){
     $model = WaxEvent::data();
