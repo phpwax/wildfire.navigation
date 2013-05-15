44e7f9b0d1432261bc107c570a4df07a4550e7c4
Sheldon Els <sheldon.els@gmail.com>
2013-05-15 17:21:07 +0100

    basic edit view

--- lib/model/WildfireNavigationItem.php
+++ lib/model/WildfireNavigationItem.php
@@ -9,6 +9,7 @@ class WildfireNavigationItem extends WildfireContent{
     //unset all the extra stuff
     unset($this->columns['content'],
           $this->columns['view'],
+          $this->columns['language'],
           $this->columns['layout'],
           $this->columns['date_start'],
           $this->columns['date_end'],
@@ -16,26 +17,30 @@ class WildfireNavigationItem extends WildfireContent{
           $this->columns['excerpt'],
           $this->columns['meta_keywords'],
           $this->columns['meta_description'],
-          $this->columns['files']
+          $this->columns['files'],
+          $this->columns['page_type']
           );
     //remove some the of tabs
     $this->columns['permalink'][1]['group']= false;
     $this->columns['permalink'][1]['editable']= false;
     $this->columns['status'][1]['editable'] = true;
-    $this->columns['status'][1]['group'] = false;
+    foreach(array("status", "alt_language", "sort") as $move_fields){
+      $this->columns[$move_fields][1]['group'] = 'content';
+      $this->columns[$move_fields][1]['primary_group'] = 1;
+    }
     //find the content model
 
-    $this->define("content_model_class", "CharField", array('widget'=>'HiddenInput'));
+    $this->define("content_model_class", "CharField", array('widget'=>'HiddenInput', 'group'=>"content", 'primary_group'=>1));
     WildfireNavigationItem::$content_model_class = CONTENT_MODEL;
     //choices for the content item
     $this->define("content_item", "ManyToManyField", array('target_model'=>WildfireNavigationItem::$content_model_class, 'scaffold'=>true, 'group'=>'relationships'));
     //an alternative url to use
-    $this->define("nav_url", "CharField", array('label'=>'External url'));
-    $this->define("accesskey","CharField");
+    $this->define("nav_url", "CharField", array('label'=>'External url', 'group'=>'content', 'primary_group'=>1));
+    $this->define("accesskey","CharField", array('group'=>'content', 'primary_group'=>1));
     //option to load in a partial as well
-    $this->define("extra_partial", "CharField", array('widget'=>'SelectInput', 'choices'=>$this->navigation_partials()) );
+    $this->define("extra_partial", "CharField", array('widget'=>'SelectInput', 'choices'=>$this->navigation_partials(), 'group'=>'content', 'primary_group'=>1));
 
-    $this->define("fake_split", "BooleanField");
+    $this->define("fake_split", "BooleanField", array('group'=>'content', 'primary_group'=>1));
   }
 
   public function scope_live(){
